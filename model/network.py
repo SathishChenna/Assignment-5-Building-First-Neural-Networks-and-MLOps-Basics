37f1cf88c5ed4ec699670b003ce5cb342162ab38
SathishChenna <sathishchenna.csr@gmail.com>
2024-11-21 14:28:19 +0530

    reduced parameters to <25K => ~14K

--- model/network.py
+++ model/network.py
@@ -4,17 +4,17 @@ import torch.nn as nn
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.fc1 = nn.Linear(64 * 5 * 5, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3)
+        self.fc1 = nn.Linear(16 * 5 * 5, 32)
+        self.fc2 = nn.Linear(32, 10)
 
     def forward(self, x):
         x = torch.relu(self.conv1(x))
         x = torch.max_pool2d(x, 2)
         x = torch.relu(self.conv2(x))
         x = torch.max_pool2d(x, 2)
-        x = x.view(-1, 64 * 5 * 5)
+        x = x.view(-1, 16 * 5 * 5)
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         return x 
